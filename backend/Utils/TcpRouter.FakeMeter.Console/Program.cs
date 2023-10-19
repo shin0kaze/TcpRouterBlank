c9a4de354e4d5a0522b77c8c475c4db01a9c2c94
stepagrus <stepagrus@mail.ru>
2023-10-19 11:52:00 +0600

    TcpRouter.FakeMeter.Console. Если не смог подключиться, то делает бесконечное кол-во попыток с интервалом 5 сек

--- backend/Utils/TcpRouter.FakeMeter.Console/Program.cs
+++ backend/Utils/TcpRouter.FakeMeter.Console/Program.cs
@@ -45,9 +45,23 @@ namespace TcpRouter.FakeMeterConscole
         using var _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
         var endPount = new IPEndPoint(ip, port);
-        Log($"Connecting to {ip}:{port}");
-        _socket.Connect(endPount);
-        Log("Connected");
+        bool isConnected = false;
+
+        while (!isConnected)
+        {
+          try
+          {
+            Log($"Connecting to {ip}:{port}");
+            _socket.Connect(endPount);
+            isConnected = true;
+            Log("Connected!");
+          }
+          catch
+          {
+            Log("Fail. I wait 5 seconds and try again");
+            Thread.Sleep(5000);
+          }
+        }
 
         var buffer = new byte[100];
         var received = Receive(_socket, buffer);
