48423018f6c61bea42c364ad81eb1a73c4abcddc
shin0kaze <Kirill.moskva@mail.ru>
2023-11-27 12:11:14 -0500

    CRUD + пара тестов

--- backend/TcpRouter.RouteManager/RouteManager.cs
+++ backend/TcpRouter.RouteManager/RouteManager.cs
@@ -1,41 +1,67 @@
 using System.Diagnostics.CodeAnalysis;
+
 using TcpRouter.Abstractions;
 
-namespace TcpRouter.RouteManager
-{
-  /// <summary>
-  /// Менеджер маршрутов
-  /// </summary>
-  public class RouteManager : IRouteMangerForApi, IRouteManagerForRouter
-  {
-    public void CreateNewRoute(Route route)
-    {
-      throw new NotImplementedException();
-    }
+namespace TcpRouter.RouteManager {
+	/// <summary>
+	/// Менеджер маршрутов
+	/// </summary>
+	public class RouteManager : IRouteMangerForApi, IRouteManagerForRouter {
+		public void CreateNewRoute(Route route)
+		{
+			if (RouteStorage.ContainsKey(route.SerialNumber))
+				throw new RouteManagerException("Route with this serial already exists");
+			if (endPoints.Contains(route.EndPoint))
+				throw new RouteManagerException("Endpoint is occupied");
 
-    public void DeleteRoute(SerialNumber route)
-    {
-      throw new NotImplementedException();
-    }
+			RouteStorage.Add(route.SerialNumber, new(route, false, false));
+			endPoints.Add(route.EndPoint);
+		}
 
-    public IReadOnlyList<RouteWithStatus> GetAllRoutes()
-    {
-      throw new NotImplementedException();
-    }
+		public void DeleteRoute(SerialNumber serial)
+		{
+			var route = RouteStorage[serial];
+			if (route is null)
+				throw new RouteManagerException("Route with this serial not found");
+			endPoints.Remove(route.Route.EndPoint);
+			RouteStorage.Remove(serial);
+		}
 
-    public void SetRouteStatus(SerialNumber serialNumber, bool isDeviceConnected, bool isConnectedToDAS)
-    {
-      throw new NotImplementedException();
-    }
+		public IReadOnlyList<RouteWithStatus> GetAllRoutes()
+		{
+			return RouteStorage.Values.ToList();
+		}
 
-    public bool TryGetRoute(SerialNumber serialNumber, [MaybeNullWhen(false)] out Route route)
-    {
-      throw new NotImplementedException();
-    }
+		public void SetRouteStatus(SerialNumber serialNumber, bool isDeviceConnected, bool isConnectedToDAS)
+		{
+			var route = RouteStorage[serialNumber];
+			RouteStorage[serialNumber] = new RouteWithStatus(route.Route, isDeviceConnected, isConnectedToDAS);
+		}
 
-    public void UpdateRoute(SerialNumber serialNumber, Route newState)
-    {
-      throw new NotImplementedException();
-    }
-  }
+		public bool TryGetRoute(SerialNumber serialNumber, [MaybeNullWhen(false)] out Route route)
+		{
+			RouteStorage.TryGetValue(serialNumber, out var routeWithStatus);
+			return (route = routeWithStatus?.Route) != null;
+		}
+
+		public void UpdateRoute(SerialNumber serialNumber, Route newState)
+		{
+			if (!RouteStorage.ContainsKey(serialNumber))
+				throw new RouteManagerException("Route with this serial not found");
+			if (endPoints.Contains(newState.EndPoint))
+				throw new RouteManagerException("Endpoint is occupied");
+
+			var routeWithStatus = RouteStorage[serialNumber];
+			endPoints.Remove(routeWithStatus.Route.EndPoint);
+			endPoints.Add(newState.EndPoint);
+			RouteStorage[serialNumber] = new RouteWithStatus(
+				newState,
+				routeWithStatus.IsDeviceConnected,
+				routeWithStatus.IsRouterConnectedToSSD);
+		}
+
+		private Dictionary<SerialNumber, RouteWithStatus> RouteStorage = new();
+		private HashSet<ReadOnlyIPEndPoint> endPoints = new();
+
+	}
 }
--- backend/Tests/TcpRouter.RouteManager.Tests/UnitTest1.cs
+++ backend/Tests/TcpRouter.RouteManager.Tests/UnitTest1.cs
@@ -1,16 +1,49 @@
-namespace TcpRouter.RouteManager.Tests
-{
-  public class Tests
-  {
-    [SetUp]
-    public void Setup()
-    {
-    }
+using TcpRouter.Abstractions;
 
-    [Test]
-    public void Test1()
-    {
-      Assert.Pass();
-    }
-  }
+namespace TcpRouter.RouteManager.Tests {
+	public class Tests {
+		public RouteManager RouteManager { get; set; }
+
+		[SetUp]
+		public void Setup()
+		{
+			RouteManager = new();
+		}
+
+		[Test]
+		public void add_route()
+		{
+			var expectedRoute = new Route(new("00001111222214"), new(ReadOnlyIPAddress.Parse("127.0.0.1"),6389));
+
+			RouteManager.CreateNewRoute(expectedRoute);
+			RouteManager.TryGetRoute(expectedRoute.SerialNumber, out var actualRoute);
+
+			Assert.AreEqual(expectedRoute, actualRoute);
+		}
+
+		[Test]
+		public void get_all_routes()
+		{
+
+			List<Route> initRoutes = new()
+		{
+		new (new("00001111222214"), new(ReadOnlyIPAddress.Parse("127.0.0.1"), 6389)),
+		new (new("10001111222214"), new(ReadOnlyIPAddress.Parse("127.0.0.2"), 6389)),
+		new (new("20001111222214"), new(ReadOnlyIPAddress.Parse("127.0.0.3"), 6389)),
+		};
+			List<RouteWithStatus> expectedRoutes = new()
+		{
+		new (initRoutes[0], false, false),
+		new (initRoutes[1], false, false),
+		new (initRoutes[2], false, false),
+		};
+			RouteManager.CreateNewRoute(initRoutes[0]);
+			RouteManager.CreateNewRoute(initRoutes[1]);
+			RouteManager.CreateNewRoute(initRoutes[2]);
+
+			var actualRoutes = RouteManager.GetAllRoutes();
+			Assert.True(actualRoutes.SequenceEqual(expectedRoutes));
+			//Assert.AreEqual(expectedRoutes, actualRoutes);
+		}
+	}
 }
